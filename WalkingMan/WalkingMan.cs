7aba6594dd539f3efef303729ee0f8bb5dbb0455
matthew-brean <matthew.brean@ocsbstudent.ca>
2015-10-08 14:21:47 -0400

    This commit makes the actual image move from right to left.

--- WalkingMan/WalkingMan.cs
+++ WalkingMan/WalkingMan.cs
@@ -27,42 +27,52 @@ namespace WalkingMan
                 if (loopCounter == 0)
                 {
                     this.picMan.Image = WalkingMan.Properties.Resources.walk2;
+                    this.picMan.Left = this.picMan.Left - 10;
                 }
                 else if (loopCounter == 1)
                 {
                     this.picMan.Image = WalkingMan.Properties.Resources.walk3;
+                    this.picMan.Left = this.picMan.Left - 5;
                 }
                 else if (loopCounter == 2)
                 {
                     this.picMan.Image = WalkingMan.Properties.Resources.walk4;
+                    this.picMan.Left = this.picMan.Left - 5;
                 }
                 else if (loopCounter == 3)
                 {
                     this.picMan.Image = WalkingMan.Properties.Resources.walk5;
+                    this.picMan.Left = this.picMan.Left - 5;
                 }
                 else if (loopCounter == 4)
                 {
                     this.picMan.Image = WalkingMan.Properties.Resources.walk6;
+                    this.picMan.Left = this.picMan.Left - 5;
                 }
                 else if (loopCounter == 5)
                 {
                     this.picMan.Image = WalkingMan.Properties.Resources.walk7;
+                    this.picMan.Left = this.picMan.Left - 5;
                 }
                 else if (loopCounter == 6)
                 {
                     this.picMan.Image = WalkingMan.Properties.Resources.walk8;
+                    this.picMan.Left = this.picMan.Left - 5;
                 }
                 else if (loopCounter == 7)
                 {
                     this.picMan.Image = WalkingMan.Properties.Resources.walk9;
+                    this.picMan.Left = this.picMan.Left - 5;
                 }
                 else if (loopCounter == 8)
                 {
                     this.picMan.Image = WalkingMan.Properties.Resources.walk10;
+                    this.picMan.Left = this.picMan.Left - 5;
                 }
                 else if (loopCounter == 9)
                 {
                     this.picMan.Image = WalkingMan.Properties.Resources.walk1;
+                    this.picMan.Left = this.picMan.Left - 5;
                 }
 
                 loopCounter = loopCounter + 1;
