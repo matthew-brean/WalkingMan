5a9248eef8b51960d53f1807c6cf5e88393272cd
matthew-brean <matthew.brean@ocsbstudent.ca>
2015-10-08 14:11:10 -0400

    First commit.

---
+++ WalkingMan/WalkingMan.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WalkingMan
+{
+    partial class frmWalkingMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picMan = new System.Windows.Forms.PictureBox();
+            this.btnWalk = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picMan
+            // 
+            this.picMan.Image = global::WalkingMan.Properties.Resources.walk1;
+            this.picMan.Location = new System.Drawing.Point(67, 60);
+            this.picMan.Name = "picMan";
+            this.picMan.Size = new System.Drawing.Size(180, 180);
+            this.picMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picMan.TabIndex = 0;
+            this.picMan.TabStop = false;
+            // 
+            // btnWalk
+            // 
+            this.btnWalk.Location = new System.Drawing.Point(112, 246);
+            this.btnWalk.Name = "btnWalk";
+            this.btnWalk.Size = new System.Drawing.Size(75, 23);
+            this.btnWalk.TabIndex = 1;
+            this.btnWalk.Text = "Walk";
+            this.btnWalk.UseVisualStyleBackColor = true;
+            this.btnWalk.Click += new System.EventHandler(this.btnWalk_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(312, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // frmWalkingMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 290);
+            this.Controls.Add(this.btnWalk);
+            this.Controls.Add(this.picMan);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmWalkingMan";
+            this.Text = "WalkingMan";
+            ((System.ComponentModel.ISupportInitialize)(this.picMan)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picMan;
+        private System.Windows.Forms.Button btnWalk;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+    }
+}
+
---
+++ WalkingMan/WalkingMan.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+// for pause
+using System.Threading;
+
+namespace WalkingMan
+{
+    public partial class frmWalkingMan : Form
+    {
+        public frmWalkingMan()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWalk_Click(object sender, EventArgs e)
+        {
+            int loopCounter = 0;
+
+            do
+            {
+                if (loopCounter == 0)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk2;
+                }
+                else if (loopCounter == 1)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk3;
+                }
+                else if (loopCounter == 2)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk4;
+                }
+                else if (loopCounter == 3)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk5;
+                }
+                else if (loopCounter == 4)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk6;
+                }
+                else if (loopCounter == 5)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk7;
+                }
+                else if (loopCounter == 6)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk8;
+                }
+                else if (loopCounter == 7)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk9;
+                }
+                else if (loopCounter == 8)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk10;
+                }
+                else if (loopCounter == 9)
+                {
+                    this.picMan.Image = WalkingMan.Properties.Resources.walk1;
+                }
+
+                loopCounter = loopCounter + 1;
+
+                // pause
+                this.Refresh();
+                Thread.Sleep(100);
+
+            } while (loopCounter < 10);
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
